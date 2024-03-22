844c89286b1c1135a5e3f39836213fc7543392f2
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-03-22 07:06:08 -0700

    Execute a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,5 @@
 # Create a manifest that kills a process named killmenow using Puppet
 exec { 'killmenow':
-  command     => '/usr/bin/pkill -f killmenow',
-  path        => '/usr/bin',
-  onlyif      => '/usr/bin/pgrep -f killmenow',
-  logoutput   => 'true',
-  refreshonly => 'true',
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
