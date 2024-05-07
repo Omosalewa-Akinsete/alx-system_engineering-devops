2222cad01423ffe6b4cc267fde4e7cf7bc43e2fe
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-05-07 05:09:29 -0700

    How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """A route that returns the number of subscribers"""
+    req = requests.get(
+            "https://www.reddit.com/r/{}/about.json".format(subreddit),
+            headers={"User-Agent": "Custom"},
+    )
+
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
+    else:
+        return 0
