5f96c2dc344c2ed129cdfe3d2af6cfbb171480a2
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-05-07 07:15:29 -0700

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,10 +9,9 @@ import requests
 def number_of_subscribers(subreddit):
     """A route that returns the number of subscribers"""
     req = requests.get(
-            "https://www.reddit.com/r/{}/about.json".format(subreddit),
-            headers={"User-Agent": "Custom"},
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
     )
-
     if req.status_code == 200:
         return req.json().get("data").get("subscribers")
     else:
