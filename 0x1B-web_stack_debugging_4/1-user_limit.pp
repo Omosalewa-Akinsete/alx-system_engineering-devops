1984dd29ede70e7f0367b387b26e5fe8c8cebfe0
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-05-16 04:34:12 -0700

    User limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,10 @@
 # Change the OS configuration so that it is possible to login with the holberton user and open a file without any error message
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin'
+exec { 'update soft limit':
+  command  => "sed -i 's/^holberton soft nofile.*/holberton soft nofile 8192/' /etc/security/limits.conf",
+  provider => 'shell',
 }
 
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+exec { 'update hard limit':
+  command  => "sed -i 's/^holberton hard nofile.*/holberton hard nofile 8192/' /etc/security/limits.conf",
+  provider => 'shell',
 }
